9c27e84b023088bbfe705cc06b2f2707533fbbd3
midhusudhakar <midhun@zeefasys.com>
2019-03-19 18:21:34 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React pls
+            Learn React
           </a>
         </header>
       </div>
